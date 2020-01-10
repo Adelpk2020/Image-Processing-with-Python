9fede9707721b41f4332d55b8e465cb76d56842a
Ahmed Abdulrahman <31901649+Adelpk2020@users.noreply.github.com>
2020-01-10 14:45:59 +0200

    Add files via upload

--- histogram.py
+++ histogram.py
@@ -1,30 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov 13 09:53:36 2018
+import numpy as np
 
-@author: ashra
-"""
+def histogram(img):
+    height = img.shape[0]
+    width = img.shape[1]
+    
+    hist = np.zeros((256))
 
-import cv2
-import matplotlib.pyplot as plt
-import numpy 
-
-
-img=cv2.imread('melgy.jpg',0)
-h=img.shape[0]
-w=img.shape[1]
-
-biggest = numpy.amax(img)
-x= [biggest]
-for i in range(biggest):
-    x.append(0)
-for i in range (h):
-    for j in range (w):
-        x[img[i][j]]+=1
-f=numpy.zeros((256,1000))
-h=f.shape[0]
-w=f.shape[1]
-for i in range (len(x)):
-        f[i,0:x[i]]=255
-plt.imshow(f)
-plt.show(f)  
+    for i in np.arange(height):
+        for j in np.arange(width):
+            a = img.item(i,j)
+            hist[a] += 1
+            
+    return hist
